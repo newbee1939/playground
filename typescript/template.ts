dc2e8bd6184864ba3ab04081a12beaf39cf25fd6
h6o <48004352+newbee1939@users.noreply.github.com>
2026-07-26 16:41:02 +0900

    テンプレートを更新し、BMI計算を要素の数と列の要素の出力に変更

--- typescript/template.ts
+++ typescript/template.ts
@@ -2,16 +2,14 @@ import * as fs from 'fs';
 
 function main() {
   const input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split(/\s+/);
-  const height = parseInt(input[0]) / 100;
-  const weight = parseInt(input[1]);
-
-  const bmi = weight / height / height;
-
-  if (bmi >= 25) {
-    console.log("Yes");
-  } else {
-    console.log("No");
-  }
+  // 要素の数
+  const n = parseInt(input[0]);
+  // 1列目の要素
+  const p = input[1];
+  console.log(p);
+  // 2列目の要素
+  const q = input[2];
+  console.log(q);
 }
 
 main();
