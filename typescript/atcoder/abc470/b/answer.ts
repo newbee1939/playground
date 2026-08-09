5fb560ccb85e9cffc8377b069032f53ef381aaa0
h6o <48004352+newbee1939@users.noreply.github.com>
2026-08-10 08:44:38 +0900

    update

--- typescript/atcoder/abc470/b/answer.ts
+++ typescript/atcoder/abc470/b/answer.ts
@@ -2,7 +2,17 @@ import * as fs from 'fs';
 
 function main() {
   const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
-  console.log(input[0]);
+  const n = Number(input[0]);
+  const numbers = input.slice(1).map(Number);
+  const counter = new Map<number, number>(); // 型を付ける（get が number|undefined になる）
+  for (const num of numbers) {
+    // has/get/elseを1行で書ける
+    counter.set(num, (counter.get(num) ?? 0) + 1);
+  }
+
+  // Math.maxで最大値を抽出できる
+  const max = Math.max(...counter.values());
+  console.log(n - max);
 }
 
 main();
